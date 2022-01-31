666cd8da9bc8e50968338e02b1ba11a427832e5d
ali090333 <88194423+ali090333@users.noreply.github.com>
2022-01-31 19:00:59 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+pkg update
+pkg upgrade
+pkg install nano -y
+pkg install tor -y
+pkg install adb -y
+pkg install mpv -y
+pkg install git -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install termux-tool -y
+pkg install bash -y
+pkg install root-repo -y
+pkg install x11-repo -y
+apt update -y
+apt upgrade -y
+pkg install fish
+pkg install wget -y
+pkg install nmap -y
+git clone https://github.com/khansaad1275/Termux-YTD
+cd Termux-YTD
+bash install.sh -y -y
+
